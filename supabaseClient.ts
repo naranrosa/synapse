24e946768fe46d692869ac8d4af1e1e802784ef2
naranrosa <natan.rodrigues10@gmail.com>
2025-09-05 13:59:16 -0300

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,7 +1,7 @@
 // src/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://yymfevuotlpuiphoxqct.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5bWZldnVvdGxwdWlwaG94cWN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcwODUwNDEsImV4cCI6MjA3MjY2MTA0MX0.ZohAonNmnLFq9Y8mUjnfA0Gpkx2fKiIcmeu0CFlvQeA';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
